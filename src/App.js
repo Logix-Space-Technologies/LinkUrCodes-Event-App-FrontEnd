9c8812ba6f17c74a371cce39a42477c4c00d7bf2
Mahadevan1234 <mahi17700@gmail.com>
2024-04-03 13:53:31 +0530

    adminlogin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/Homepage/HomePage';
+import AdminLogin from './components/Admin/AdminLogin';
 
 function App() {
   return (
     <div>
-     
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/adminlogin' element={<AdminLogin/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Admin/AdminLogin.jsx
+++ src/components/Admin/AdminLogin.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AdminLogin = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <center>
+                <div className='login'>
+                    <div className='card'>
+                        <div className="container">
+                            <div className="row g-3">
+                                <div class="card-body">
+                                    <h2 class="card-title"><u>ADMIN LOGIN</u></h2><br></br>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 ">
+                                        <div className="row g-3">
+                                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                                <label htmlFor="" className="form-label">EMAIL-ID</label>
+                                                <input type="text" className="form-control" />
+                                            </div>
+                                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                                <label htmlFor="" className="form-label">PASSWORD</label>
+                                                <input type="password" className="form-control" />
+                                            </div>
+                                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                                <button className="btn btn-primary">LOGIN</button>
+                                            </div>
+                                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                                <center><Link to="/" className="nav-link">Back to Home</Link></center>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+    )
 }
 
 export default AdminLogin
