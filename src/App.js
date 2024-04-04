acaa9df463d648d63dc76a57bc407b5bd7651c04
ArunKuriakose15 <arunkp1515@gmail.com>
2024-04-04 20:25:32 +0530

    user home page

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import StudentLogin from './components/Student/StudentLogin';
 import CollegeLogin from './components/College/CollegeLogin';
 import CollegeHomePage from './components/College/CollegeHomePage';
 
-import UserLogin from './components/User/UserLogin';
+
 import AdminHomePage from './components/Admin/AdminHomePage';
 import StudentHomePage from './components/Student/StudentHomePage';
+import UserHomePage from './components/User/UserHomePage';
+import UserLogin from './components/user/UserLogin';
 
 
 
@@ -28,6 +30,7 @@ function App() {
         <Route path='/userlogin' element={<UserLogin/>}/>
         <Route path='/adminhome' element={<AdminHomePage/>}/>
         <Route path='/studenthome' element={<StudentHomePage/>}/>
+        <Route path='/userhome' element={<UserHomePage/>}/>
 
       </Routes>
       </BrowserRouter>
---
+++ src/components/User/UserNavBar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const UserNavBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-tertiary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="#"><b>Link Ur Codes</b></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/userhome">Home</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Sign in
+                                </Link>
+                                <ul className="dropdown-menu">
+                                    <li><Link className="dropdown-item" to="/adminlogin">Admin Login</Link></li>
+
+                                    <li><Link className="dropdown-item" to="/collegelogin">College Login</Link></li>
+
+                                    <li><Link className="dropdown-item" to="/userlogin">User Login</Link></li>
+
+                                    <li><Link className="dropdown-item" to="/studentlogin">Student Login</Link></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default UserNavBar
