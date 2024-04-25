5ad0c5b3e3daef8e296aa9a5c7d2339c6bb3e8e0
Mahadevan1234 <mahi17700@gmail.com>
2024-04-25 13:14:13 +0530

    public event view

--- src/components/Admin/ViewPublicEvent.jsx
+++ src/components/Admin/ViewPublicEvent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import AdminNavbar from './AdminNavbar'
-import { axios } from 'axios'
+import axios from 'axios'
 
 const ViewPublicEvent = () => {
     const [data, setData] = new useState([])
