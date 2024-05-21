c01bb1e12b5b7da348e4ec9cd32cef0510c8319c
Mahadevan1234 <mahi17700@gmail.com>
2024-05-21 17:45:46 +0530

    admin add events

--- src/components/Admin/AddPublicEvent.jsx
+++ src/components/Admin/AddPublicEvent.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminNavbar from './AdminNavbar'
+import axios from 'axios'
 
 const AddPublicEvent = () => {
+    const [input,setInput]=useState(
+        {
+            "event_public_name":"",
+            "event_public_amount":"",
+            "event_public_description":"",
+            "event_public_date":"",
+            "event_public_time":"",
+            "event_public_image":"",
+            "event_public_syllabus":"",
+            "event_public_venue":"",
+            "event_addedby":sessionStorage.getItem("adminid")
+        }
+    )
+    const inputHandler=(event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+    }
+    const readValues=()=>{
+        axios.post("http://localhost:8085/api/events/add_public_events",input, {}, { headers: { token: sessionStorage.getItem("admintoken") } }).then(
+            (response)=>{
+                if (response.data.status === "success") {
+                    alert("Successfully added")
+                    setInput(
+                        {
+                            "event_public_name":"",
+                            "event_public_amount":"",
+                            "event_public_description":"",
+                            "event_public_date":"",
+                            "event_public_time":"",
+                            "event_public_image":"",
+                            "event_public_syllabus":"",
+                            "event_public_venue":"",
+                            "event_addedby":sessionStorage.getItem("adminid")
+                        }
+                    )
+                }
+                else if(response.data.status==="Unauthorized user"){
+                    alert("Unauthorized access")
+                }
+                else {
+                    alert("Something went wrong")
+                    setInput(
+                        {
+                            "event_public_name":"",
+                            "event_public_amount":"",
+                            "event_public_description":"",
+                            "event_public_date":"",
+                            "event_public_time":"",
+                            "event_public_image":"",
+                            "event_public_syllabus":"",
+                            "event_public_venue":"",
+                            "event_addedby":sessionStorage.getItem("adminid")
+                        }
+                    )
+                }
+
+            }
+        )
+    }
   return (
     <div>
         <AdminNavbar/>
@@ -9,34 +68,38 @@ const AddPublicEvent = () => {
                 <div className="row g-3">
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="name" className="form-label">Event Name</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='event_public_name' value={input.event_public_name} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="amount" className="form-label">Event Amount</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='event_public_amount' value={input.event_public_amount} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="description" className="form-label">Event Description</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='event_public_description' value={input.event_public_description} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="date" className="form-label">Event Date</label>
-                        <input type="date" className="form-control" />
+                        <input type="date" className="form-control" name='event_public_date' value={input.event_public_date} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="time" className="form-label">Event Time</label>
-                        <input type="time" className="form-control" />
+                        <input type="time" className="form-control" name='event_public_time' value={input.event_public_time} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="image" className="form-label">Event Image</label>
-                        <input type="file" className="form-control" />
+                        <input type="file" className="form-control" name='event_public_image' value={input.event_public_image} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="syllabus" className="form-label">Event Syllabus</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='event_public_syllabus' value={input.event_public_syllabus} onChange={inputHandler} />
                     </div>
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <button className="btn btn-success">Add Event</button>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="venue" className="form-label">Event Venue</label>
+                        <input type="text" className="form-control" name='event_public_venue' value={input.event_public_venue} onChange={inputHandler} />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <button className="btn btn-success" onClick={readValues}>Add Event</button>
                     </div>
                 </div>
             </div>
