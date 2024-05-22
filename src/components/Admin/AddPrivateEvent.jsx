855014af33fdaf6e10c67c12039eea425099779a
Mahadevan1234 <mahi17700@gmail.com>
2024-05-22 16:21:33 +0530

    admin add private events

--- src/components/Admin/AddPrivateEvent.jsx
+++ src/components/Admin/AddPrivateEvent.jsx
@@ -1,38 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminNavbar from './AdminNavbar'
 
 const AddPrivateEvent = () => {
+    const [input, setInput] = useState({
+        event_private_name: "",
+        event_private_amount: "",
+        event_private_description: "",
+        event_private_date: "",
+        event_private_time: "",
+        image: null,
+        event_addedby: sessionStorage.getItem("adminid")
+    });
+
+    const inputHandler = (event) => {
+        const { name, value, files } = event.target;
+        if (name === 'image') {
+            setInput({ ...input, [name]: files[0] });
+        } else {
+            setInput({ ...input, [name]: value });
+        }
+    };
+
+    const readValues = () => {
+        const formData = new FormData();
+        for (const key in input) {
+            formData.append(key, input[key]);
+        }
+
+        axios.post("http://localhost:8085/api/events/add_private_events", formData, {
+            headers: {
+                token: sessionStorage.getItem("admintoken"),
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then((response) => {
+            if (response.data.status === "success") {
+                alert("Successfully added");
+                setInput({
+                    event_private_name: "",
+                    event_private_amount: "",
+                    event_private_description: "",
+                    event_private_date: "",
+                    event_private_time: "",
+                    image: null,
+                    event_addedby: sessionStorage.getItem("adminid")
+                });
+            } else if (response.data.status === "Unauthorized user") {
+                alert("Unauthorized access");
+            } else {
+                alert("Something went wrong");
+                setInput({
+                    event_private_name: "",
+                    event_private_amount: "",
+                    event_private_description: "",
+                    event_private_date: "",
+                    event_private_time: "",
+                    image: null,
+                    event_addedby: sessionStorage.getItem("adminid")
+                });
+            }
+        }).catch(error => {
+            console.error('Error adding event:', error.message);
+            alert("Something went wrong" + error.message);
+        });
+    };
     return (
         <div>
-            <AdminNavbar/>
+            <AdminNavbar />
             <div className="container">
                 <div className="row g-3">
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="name" className="form-label">Event Name</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='event_private_name' value={input.event_private_name} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="amount" className="form-label">Event Amount</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='event_private_amount' value={input.event_private_amount} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="description" className="form-label">Event Description</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name='event_private_description' value={input.event_private_description} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="date" className="form-label">Event Date</label>
-                        <input type="date" className="form-control" />
+                        <input type="date" className="form-control" name='event_private_date' value={input.event_private_date} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="time" className="form-label">Event Time</label>
-                        <input type="time" className="form-control" />
+                        <input type="time" className="form-control" name='event_private_time' value={input.event_private_time} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="image" className="form-label">Event Image</label>
-                        <input type="file" className="form-control" />
+                        <input type="file" className="form-control" name='image' onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <button className="btn btn-success">Add Event</button>
+                        <button className="btn btn-success" onClick={readValues}>Add Event</button>
                     </div>
                 </div>
             </div>
