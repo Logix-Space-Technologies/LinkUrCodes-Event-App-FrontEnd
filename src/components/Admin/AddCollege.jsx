e670f6f635da98182c284dddba89b8288986faa0
Mahadevan1234 <mahi17700@gmail.com>
2024-05-22 16:52:48 +0530

    admin add college

---
+++ src/components/Admin/AddCollege.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import AdminNavbar from './AdminNavbar'
+
+const AddCollege = () => {
+    return (
+        <div>
+            <AdminNavbar/>
+            <div className="container">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="name" className="form-label">College Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="name" className="form-label">College Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="name" className="form-label">College Phone</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="image" className="form-label">College Image</label>
+                        <input type="file" className="form-control" name='image'/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">Add College</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddCollege
---
+++ src/components/Admin/DeleteCollege.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminNavbar from './AdminNavbar'
+
+const DeleteCollege = () => {
+  return (
+    <div>
+      <AdminNavbar/>
+    </div>
+  )
+}
+
+export default DeleteCollege
