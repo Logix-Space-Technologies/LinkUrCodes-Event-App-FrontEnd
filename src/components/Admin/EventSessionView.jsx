80995b9ecc48d12ff003b39c948f7a0fe6f80027
Mahadevan1234 <mahi17700@gmail.com>
2024-06-04 01:27:41 +0530

    view session

--- src/components/Admin/EventSessionView.jsx
+++ src/components/Admin/EventSessionView.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AdminNavbar from './AdminNavbar'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const EventSessionView = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [data,setData]=useState([])
+    const getData=()=>{
+        axios.post("http://localhost:8085/api/events/viewSession",{event_private_id:sessionStorage.getItem("eventID")},{headers:{token: sessionStorage.getItem("admintoken")}})
+        .then((response)=>{
+            setData(response.data)
+        })
+        .catch((error)=>{
+            console.error('Error fetching data:', error)
+        })
+    }
+    useEffect(()=>{getData()},[])
+    return (
+        <div>
+            <AdminNavbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Session Name</th>
+                                    <th scope="col">Session Date</th>
+                                    <th scope="col">Session Time</th>
+                                    <th scope="col">Session Type</th>
+                                    <th scope="col">Session Venue</th>
+                                    <th scope="col">Session Attendance</th>
+                                    <th scope="col">Session Iscompleted</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map((value,index)=>{
+                                        return <tr>
+                                        <td>{index+1}</td>
+                                        <td>{value.session_topic_description}</td>
+                                        <td>{value.session_date}</td>
+                                        <td>{value.session_start_time}</td>
+                                        <td>{value.type}</td>
+                                        <td>{value.venue}</td>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                    </tr>
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        <Link className="link" to="/viewprivateevent">Back to events</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default EventSessionView
