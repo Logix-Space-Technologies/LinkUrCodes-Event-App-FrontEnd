d085ea4a863a20425c83151c7edbb81b07b3b2a8
taniya-ignatius <itaniya895@gmail.com>
2024-06-04 15:09:54 +0530

    Student sessions

---
+++ src/components/Student/ForgotPassword.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ForgotPassword = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        To help you recover your account, Link Ur Codes can send a verification code to your registered account.
+                        To get a sign-in code, confirm your mail id:
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ForgotPassword
