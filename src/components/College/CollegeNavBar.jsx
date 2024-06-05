1cca554b04caa357e21b8b21bb7b0814401d9fa7
Mahadevan1234 <mahi17700@gmail.com>
2024-06-05 19:34:06 +0530

    student navbar

--- src/components/College/CollegeNavBar.jsx
+++ src/components/College/CollegeNavBar.jsx
@@ -39,7 +39,7 @@ const CollegeNavBar = () => {
                 <Link className="nav-link" to="/collegehome">Event History</Link>
               </li>
               <li className="nav-item">
-                <span className='nav-link' onClick={logOutAction}> Logout</span>
+                <button className='nav-link' onClick={logOutAction}> Logout</button>
               </li>
             </ul>
           </div>
--- src/components/Student/StudentHomePage.jsx
+++ src/components/Student/StudentHomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import StudentNavBar from './StudentNavBar'
+import Slider from '../College/Slider'
 
 const StudentHomePage = () => {
   return (
     <div>
         <StudentNavBar/>
+        <Slider/>
     </div>
   )
 }
