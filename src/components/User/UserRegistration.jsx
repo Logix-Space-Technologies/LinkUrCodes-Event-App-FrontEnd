5a2a5e15192693231ae56bbc4cecef50fa68937f
taniya-ignatius <itaniya895@gmail.com>
2024-06-06 11:14:20 +0530

    User Registration successfull

--- src/components/User/UserRegistration.jsx
+++ src/components/User/UserRegistration.jsx
@@ -1,96 +1,115 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import HomeNavbar from '../Homepage/HomeNavbar'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import HomeNavbar from '../Homepage/HomeNavbar';
 
 const UserRegistration = () => {
-    const navigate=useNavigate()
-    const [input, setInput] = useState(
-        {
-            "user_name": "",
-            "user_image": "",
-            "user_email": "",
-            "user_password": "",
-            "user_contact_no": "",
-            "user_qualification": "",
-            "user_skills": ""
-        }
-    )
+    const navigate = useNavigate();
+    const [input, setInput] = useState({
+        user_name: "",
+        user_image: null, 
+        user_email: "",
+        user_password: "",
+        user_contact_no: "",
+        user_qualification: "",
+        user_skills: ""
+    });
+
     const inputHandler = (event) => {
-        setInput({ ...input, [event.target.name]: event.target.value })
-    }
+        const { name, value, files } = event.target;
+        if (name === 'user_image') {
+            setInput({ ...input, user_image: files[0] }); 
+        } else {
+            setInput({ ...input, [name]: value });
+        }
+    };
+
     const readValues = () => {
-        console.log(input)
-        axios.post("http://localhost:8085/api/users/signup", input).then(
-            (response) => {
-                if (response.data.status === "success") {
-                    alert("Successfully registered")
-                    navigate('/userlogin')
-                    setInput(
-                        {
-                            "user_name": "",
-                            "user_image": "",
-                            "user_email": "",
-                            "user_password": "",
-                            "user_contact_no": "",
-                            "user_qualification": "",
-                            "user_skills": ""
-                        }
-                    )
-                }
-                else {
-                    alert("Something went wrong")
-                    setInput(
-                        {
-                            "user_name": "",
-                            "user_image": "",
-                            "user_email": "",
-                            "user_password": "",
-                            "user_contact_no": "",
-                            "user_qualification": "",
-                            "user_skills": ""
-                        }
-                    )
-                }
+        const formData = new FormData();
+        formData.append('user_name', input.user_name);
+        formData.append('image', input.user_image); 
+        formData.append('user_email', input.user_email);
+        formData.append('user_password', input.user_password);
+        formData.append('user_contact_no', input.user_contact_no);
+        formData.append('user_qualification', input.user_qualification);
+        formData.append('user_skills', input.user_skills);
+
+        axios.post("http://localhost:8085/api/users/signup", formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
             }
-        )
-    }
+        }).then((response) => {
+            if (response.data.status === "success") {
+                alert("Successfully registered");
+                navigate('/userlogin');
+                setInput({
+                    user_name: "",
+                    user_image: null,
+                    user_email: "",
+                    user_password: "",
+                    user_contact_no: "",
+                    user_qualification: "",
+                    user_skills: ""
+                });
+            } else {
+                alert("Something went wrong");
+                setInput({
+                    user_name: "",
+                    user_image: null,
+                    user_email: "",
+                    user_password: "",
+                    user_contact_no: "",
+                    user_qualification: "",
+                    user_skills: ""
+                });
+            }
+        }).catch((error) => {
+            alert(`Error: ${error.response?.data?.message || 'Something went wrong'}`);
+            setInput({
+                user_name: "",
+                user_image: null,
+                user_email: "",
+                user_password: "",
+                user_contact_no: "",
+                user_qualification: "",
+                user_skills: ""
+            });
+        });
+    };
+
     return (
         <div>
-            <HomeNavbar/>
-            <br></br>
+            <HomeNavbar />
+            <br />
             <div className="bg-image">
                 <div className="container">
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <div className="col col-6 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-
-                                <img src="https://i.pinimg.com/564x/aa/01/30/aa01306d3a0426c2125191caf2f955bd.jpg" height="665px" width="1000px" class="card-img-top" alt="..." />
-
+                                <img src="https://i.pinimg.com/564x/aa/01/30/aa01306d3a0426c2125191caf2f955bd.jpg" height="665px" width="1000px" className="card-img-top" alt="..." />
                             </div>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <div class="card h-100">
-                                <div class="card-body">
-                                    <center><b><u><h3 class="card-title">NEW USER REGISTRATION</h3></u></b></center>
-                                    <br></br>
+                            <div className="card h-100">
+                                <div className="card-body">
+                                    <center><b><u><h3 className="card-title">NEW USER REGISTRATION</h3></u></b></center>
+                                    <br />
                                     <div className="row mb-3">
                                         <label htmlFor="name" className="col-sm-4 col-form-label">NAME:</label>
                                         <div className="col-sm-8">
-                                            <input type="text" className="form-control" name="user_name" value={input.user_name} onChange={inputHandler}  />
+                                            <input type="text" className="form-control" name="user_name" value={input.user_name} onChange={inputHandler} />
                                         </div>
                                     </div>
                                     <div className="row mb-3">
                                         <label htmlFor="profileImage" className="col-sm-4 col-form-label">PROFILE IMAGE:</label>
                                         <div className="col-sm-8">
-                                            <input type="file" className="form-control" name="user_image" value={input.user_image} onChange={inputHandler}  />
+                                            <input type="file" className="form-control" name="user_image" onChange={inputHandler} />
                                         </div>
                                     </div>
                                     <div className="row mb-3">
                                         <label htmlFor="email" className="col-sm-4 col-form-label">EMAIL ID:</label>
                                         <div className="col-sm-8">
-                                            <input type="email" className="form-control" name="user_email" value={input.user_email} onChange={inputHandler}  />
+                                            <input type="email" className="form-control" name="user_email" value={input.user_email} onChange={inputHandler} />
                                         </div>
                                     </div>
                                     <div className="row mb-3">
@@ -102,7 +121,7 @@ const UserRegistration = () => {
                                     <div className="row mb-3">
                                         <label htmlFor="contact" className="col-sm-4 col-form-label">CONTACT:</label>
                                         <div className="col-sm-8">
-                                            <input type="text" className="form-control" name="user_contact_no" value={input.user_contact_no} onChange={inputHandler}/>
+                                            <input type="text" className="form-control" name="user_contact_no" value={input.user_contact_no} onChange={inputHandler} />
                                         </div>
                                     </div>
                                     <div className="row mb-3">
@@ -116,21 +135,17 @@ const UserRegistration = () => {
                                         <div className="col-sm-8">
                                             <textarea className="form-control" name="user_skills" value={input.user_skills} onChange={inputHandler}></textarea>
                                         </div>
-                                    </div><br></br><center>
+                                    </div><br />
+                                    <center>
                                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                             <button className="btn btn-primary" onClick={readValues}>REGISTER</button>
-                                        </div><br></br>
+                                        </div><br />
                                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
                                             <Link to="/userlogin" className="nav-link" style={{ color: 'blue', textDecoration: 'underline' }}>Existing Users Click Here</Link>
-
-                                        </div><br></br>
+                                        </div><br />
                                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
                                             <Link to="/" className="nav-link" style={{ color: 'blue', textDecoration: 'underline' }}>Back to home</Link>
-
                                         </div>
-
                                     </center>
                                 </div>
                             </div>
@@ -139,7 +154,7 @@ const UserRegistration = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default UserRegistration
+export default UserRegistration;
