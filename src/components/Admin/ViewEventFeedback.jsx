85f3f864528554fe5aa5ce24d2493220805c9920
Mahadevan1234 <mahi17700@gmail.com>
2024-06-06 17:25:15 +0530

    view feedback

--- src/components/Admin/ViewEventFeedback.jsx
+++ src/components/Admin/ViewEventFeedback.jsx
@@ -5,20 +5,26 @@ import { Link } from 'react-router-dom';
 
 const ViewEventFeedback = () => {
     const [data, setData] = useState([]);
-    
+
     const getData = () => {
-        axios.post("http://localhost:8085/api/feedback/viewallfeedbackuser", 
-            { feedback_id: sessionStorage.getItem("feedbackID") }, 
+        axios.post("http://localhost:8085/api/feedback/viewallfeedbackuser",
+            { feedback_id: sessionStorage.getItem("feedbackID") },
             { headers: { token: sessionStorage.getItem("admintoken") } }
         ).then((response) => {
-            if (response.data.status === "No Feedbacks Found") {
-                alert("Something went wrong !");
-                setData([]); // No feedbacks found
-            } else if (response.data.length > 0) {
-                setData(response.data);
-            } else {
-                alert("Something went wrong !");
+            if (Array.isArray(response.data.data)) {
+                setData(response.data.data);
+                console.log("da", data)
+            } else if (response.data.length === 0) {
+                setData([]);
             }
+            //if (response.data.status === "No Feedbacks Found") {
+            //alert("Something went wrong !");
+            //setData([]); // No feedbacks found
+            //} else if (response.data.length > 0) {
+            //setData(response.data);
+            // } else {
+            //alert("Something went wrong !");
+            // }
         }).catch((error) => {
             console.error("Error fetching data: ", error);
             alert("Error fetching data. Please try again later.");
@@ -29,27 +35,35 @@ const ViewEventFeedback = () => {
 
     return (
         <div>
-            <AdminNavbar/>
+            <AdminNavbar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        {data.length > 0 ? (
-                            data.map((feedback, index) => (
-                                <div className="card mb-3" key={index}>
-                                    <div className="card-body">
-                                        <h5 className="card-title">Feedback {index + 1}</h5>
-                                        <p className="card-text">{feedback.feedback_content}</p>
-                                    </div>
-                                </div>
-                            ))
-                        ) : (
-                            <div className="card">
-                                <div className="card-body">
-                                    <p>No feedback available.</p>
-                                </div>
+                        {data.length === 0 ? (
+                            <div>
+                                <center>
+                                    <h1>No feedbacks found   </h1>
+                                </center>
                             </div>
+                        ) : (
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Feedback</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data.map((feedback, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{feedback.feedback_content}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         )}
-                         <Link className="link" to="/viewprivateevent">Back to events</Link>
+                        <Link className="link" to="/viewprivateevent">Back to events</Link>
                     </div>
                 </div>
             </div>
