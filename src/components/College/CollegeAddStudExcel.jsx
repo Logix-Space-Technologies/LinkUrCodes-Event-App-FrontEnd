06e56e502cd417f1c7757c6e56a7b5959be239b5
ArunKuriakose15 <arunkp1515@gmail.com>
2024-08-02 22:46:52 +0530

    validation on excel

--- src/components/College/CollegeAddStudExcel.jsx
+++ src/components/College/CollegeAddStudExcel.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
 import CollegeNavBar from './CollegeNavBar';
 import ExampleExcelFile from '../Student_details.xlsx';
-import '../../config'
+import '../../config';
 
 const CollegeAddStudExcel = () => {
-    const apiUrl = global.config.urls.api.server + "/api/college/studentupload"
+    const apiUrl = global.config.urls.api.server + "/api/college/studentupload";
     const [file, setFile] = useState(null);
-
+    const [invalidData, setInvalidData] = useState([]);
+    const excelRef = useRef(null);
     const handleFileChange = (event) => {
         setFile(event.target.files[0]);
     };
@@ -26,9 +27,26 @@ const CollegeAddStudExcel = () => {
                     },
                 });
                 if (response.data.status === "Success") {
-                    alert("Successfully Added!");
-                } else {
+                    if (response.data.invalidData && response.data.invalidData.length > 0) {
+                        alert("Successfully Added some student details!");
+                        setInvalidData(response.data.invalidData);
+                    } else {
+                        alert("Successfully Added!");
+                        setInvalidData([]);
+                        excelRef.current.value = null;
+                    }
+                } else if(response.data.status === "error"){
+                    if (response.data.invalidData && response.data.invalidData.length > 0) {
+                        setInvalidData(response.data.invalidData);
+                    } else {
+                        alert("Something went wrong!");
+                        setInvalidData([]);
+                        excelRef.current.value = null;
+                    }
+                }
+                else{
                     alert("Something went wrong!");
+                    excelRef.current.value = null;
                 }
             } catch (error) {
                 console.error('Error uploading file:', error);
@@ -51,8 +69,8 @@ const CollegeAddStudExcel = () => {
             <CollegeNavBar />
             <div className="container">
                 <div className="row g-3">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <br></br>
+                    <div className="col col-12">
+                        <br />
                         <p>
                             To upload student data via an Excel sheet, please download the template from the link below.
                             Ensure your data matches the format specified in the template.
@@ -64,18 +82,51 @@ const CollegeAddStudExcel = () => {
                             </svg> Download Example Excel
                         </button>
                     </div>
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="col col-12">
                         <input
                             type="file"
                             className="form-control"
                             name="file"
                             onChange={handleFileChange}
+                            ref={excelRef}
                         />
                     </div>
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="col col-12">
                         <button className="btn btn-primary" onClick={handleUpload}>
                             Upload
                         </button>
+                    </div>
+                    <div className="col col-12">
+                        {invalidData.length > 0 && (
+                            <div className="alert alert-warning mt-3">
+                                <h5>Invalid Students Data</h5>
+                                <table className="table ">
+                                    <thead>
+                                        <tr class="table-warning">
+                                            <th scope="col">#</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Rollno</th>
+                                            <th scope="col">Admission no</th>
+                                            <th scope="col">Phone</th>
+                                            <th scope="col">Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {invalidData.map((student, index) => (
+                                            <tr class="table-warning">
+                                                <th>{index+1}</th>
+                                                <th>{student.student_name}</th>
+                                                <td>{student.student_rollno}</td>
+                                                <td>{student.student_admno}</td>
+                                                <td>{student.student_phone_no}</td>
+                                                <td>{student.student_email}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
